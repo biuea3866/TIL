24996843c1b471f5cd05560ea1db3cdbe1285614
biuea3866 <59189504+biuea3866@users.noreply.github.com>
2025-11-05 00:35:32 +0900

    [object] 메시지와 인터페이스 (#17)

---
+++ practice/src/main/kotlin/com/biuea/practice/message_and_interface/Movie.kt
@@ -0,0 +1,37 @@
+package com.biuea.practice.message_and_interface
+
+import java.time.Duration
+
+class Movie(
+    private var _title: String,
+    private var _runningTime: Duration,
+    private var _fee: Money,
+    private var _movieType: MovieType,
+    private var _discountConditions: List<DiscountCondition>,
+    private var _discountAmount: Money,
+    private var _discountPercent: Double
+) {
+    val fee get() = this._fee
+
+    fun calculateMovieFee(screening: Screening): Money {
+        if (this.isDiscountable(screening)) {
+            return this._fee.minus(this.calculateDiscountAmount())
+        }
+
+        return this._fee
+    }
+
+    private fun isDiscountable(screening: Screening): Boolean {
+        return _discountConditions.any { it.isSatisfiedBy(screening) }
+    }
+
+    private fun calculateDiscountAmount(): Money {
+        return Money.ZERO
+    }
+}
+
+enum class MovieType {
+    AMOUNT_DISCOUNT,
+    PERCENT_DISCOUNT,
+    NONE_DISCOUNT
+}
